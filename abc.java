5e9090c2e6b3bd9d49d5a70c0a7f27e2dcd90803
vivek varma <vivekvarms856@gmail.com>
2020-01-13 23:10:45 +0530

    Made Grading Project

---
+++ abc.java
@@ -0,0 +1,17 @@
+class abc
+{
+
+public static void main(String[] args)
+{
+ System.out.println("code to know whether a no is even or odd");
+ int a=5;
+ if(a>2)
+ {
+  System.out.println("no is greater than 2");
+}
+ else
+{
+ System.out.println("no is smaller than 2");
+}
+}
+}
